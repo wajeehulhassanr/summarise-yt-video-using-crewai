0ddc2d4f1314459e93012d1f3f4e296e66c2d134
Wajeeh Ul Hassan <86775120+wajeehulhassanr@users.noreply.github.com>
2024-06-20 17:09:47 +0500

    Update agents.py

--- agents.py
+++ agents.py
@@ -1,10 +1,19 @@
+"""
+agents.py
+
+This module defines the Agent configuration for generating notes from YouTube videos.
+The Agent uses a YouTube tool to fetch transcripts and formats the information into structured markdown notes.
+"""
+
 from crewai import Agent
 from tools import yt_tool
 
 notes_maker = Agent(
   role='Notes creater for Youtube Videos',
-  goal="""Get transcript of video specific to following topic : {video_name} and write detailed notes wiht relavant information in structured markdown""",
-  backstory="""A skilled Data Scientist who is expert in structuring important information and procedures, hired to write blogs from youtube videos""",
+  goal="""Get transcript of video specific to following topic : {video_name} and write 
+  detailed notes wiht relavant information in structured markdown""",
+  backstory="""A skilled Data Scientist who is expert in structuring important 
+  information and procedures, hired to write blogs from youtube videos""",
   tools=[yt_tool],  # Optional, defaults to an empty list
   max_rpm=10, # Optional
   verbose=True,  # Optional
