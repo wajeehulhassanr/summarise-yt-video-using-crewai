16526282b7be97d4197a776e7e5c55f4fa339341
Wajeeh Ul Hassan <86775120+wajeehulhassanr@users.noreply.github.com>
2024-06-20 17:05:14 +0500

    Update agents.py

--- agents.py
+++ agents.py
@@ -3,10 +3,8 @@ from tools import yt_tool
 
 notes_maker = Agent(
   role='Notes creater for Youtube Videos',
-  goal="""Get transcript of video specific to following topic : {video_name} and 
-  write detailed notes wiht relavant information in structured markdown""",
-  backstory="""A skilled Data Scientist who is expert in structuring important information 
-   and procedures, hired to write blogs from youtube videos""",
+  goal="""Get transcript of video specific to following topic : {video_name} and write detailed notes wiht relavant information in structured markdown""",
+  backstory="""A skilled Data Scientist who is expert in structuring important information and procedures, hired to write blogs from youtube videos""",
   tools=[yt_tool],  # Optional, defaults to an empty list
   max_rpm=10, # Optional
   verbose=True,  # Optional
